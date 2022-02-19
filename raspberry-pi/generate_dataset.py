7540c60189ccc9f89031b5478af9b7fa331920ee
Nihal Sahu <nihalsahu360@gmail.com>
2022-02-19 15:30:51 -0700

    added script to generate data of user

--- raspberry-pi/generate_dataset.py
+++ raspberry-pi/generate_dataset.py
@@ -9,15 +9,17 @@ video = cv2.VideoCapture(0)
 video.set(3, 320)
 video.set(4, 240)
 
-print("Enter the id and name of the person: ")
-userId = input()
+print("\nPress 'q' to exit, press 's' to save an image to the dataset")
+print("Enter the name of the person: ")
+
 userName = input()
 
 count = 1 #used to see how many images we have saved
 
 #method to save images
-def saveImage(img, userName, userId, imgId):
+def saveImage(img, userName, imgId):
     Path("dataset/{}".format(userName)).mkdir(parents=True, exist_ok = True)       #create folder to save images
+    cv2.imwrite("dataset/{}/{}_{}.jpg".format(userName, userName, imgId), originalFrame)
 
 while True:
 
@@ -36,8 +38,12 @@ while True:
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord('s'):
-        saveImage(originalImg, userName, userId, count)
-        count += 1
+        #save only 20 images
+        if count <= 20:
+            saveImage(originalFrame, userName, count)
+            count += 1
+        else:
+            break     
     elif key == ord('q'):
         break
 
