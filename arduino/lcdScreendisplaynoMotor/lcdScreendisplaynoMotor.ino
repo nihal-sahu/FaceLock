882f659b3eb9ae8b092259c7c2bc0f40b23b87e3
Nihal Sahu <nihal.sahu@uwaterloo.ca>
2022-02-20 01:59:22 -0700

    added lcd configuration file

--- arduino/lcdScreendisplaynoMotor/lcdScreendisplaynoMotor.ino
+++ arduino/lcdScreendisplaynoMotor/lcdScreendisplaynoMotor.ino
@@ -1,3 +1,6 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   // beginning UART communications baud rate 9600
   Serial.begin(9600);
@@ -10,8 +13,7 @@ void loop() {
   //checking if there is UART data to receive
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0, 0);
     lcd.print(data);
   }
-
 }
